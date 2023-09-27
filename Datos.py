f9704f19cb8e111bb02932fb998ae20d2f6597b8
RafaDom02 <rafadomsae@outlook.es>
2023-09-27 13:13:48 +0200

    Semana 2 casi completa

--- Datos.py
+++ Datos.py
@@ -8,8 +8,9 @@ import os
 
 class Datos:
 
-    dict = {}
+    diccionarios = {}
     nominalAtributos = []
+    datos = None
 
     # Constructor: procesar el fichero para asignar correctamente las variables nominalAtributos, datos y diccionarios
     def __init__(self, nombreFichero):  
@@ -22,45 +23,43 @@ class Datos:
         keys = list(aux_list[0].keys())
         #añadir las keys al diccionario
         for i in range(len(keys)):
-            self.dict[keys[i]] = {}
+            self.diccionarios[keys[i]] = {}
         
         for i in range(len(aux_list)):
-            for label in self.dict.keys():
-                if aux_list[i][label] not in self.dict[label]:
+            for label in self.diccionarios.keys():
+                if aux_list[i][label] not in self.diccionarios[label]:
                     if  not (type(aux_list[i][label]) == int or type(aux_list[i][label]) == float) or\
                         label.casefold() == "Class".casefold():
 
                         if i == 0:
                             self.nominalAtributos.append(True)
-                        if aux_list[i][label] not in self.dict[label].values():
-                            self.dict[label].update({str(aux_list[i][label]): None})
-                    elif (type(aux_list[i][label]) == int or type(aux_list[i][label])) and i == 0:
-                            self.nominalAtributos.append(False)
+                        if aux_list[i][label] not in self.diccionarios[label].values():
+                            self.diccionarios[label].update({str(aux_list[i][label]): None})
+                    elif (type(aux_list[i][label]) == int or type(aux_list[i][label])):
+                            if i == 0:
+                                self.nominalAtributos.append(False)
                     else:
                         raise ValueError(f"Error en el tipo de dato: {label}")
         
 
-        for label in self.dict.keys():
-            keys = list(self.dict[label].keys())
-            keys.sort()
-            self.dict[label] = {i: self.dict[label][i] for i in keys}    
-            for i,key in enumerate(self.dict[label].keys()):
-                self.dict[label][key] = i
+        for label in self.diccionarios.keys():
+            s_keys = list(self.diccionarios[label].keys())
+            s_keys.sort()
+            self.diccionarios[label] = {i: self.diccionarios[label][i] for i in s_keys}    
+            for i,key in enumerate(self.diccionarios[label].keys()):
+                self.diccionarios[label][key] = i
         
 
-        
-        print(self.dict) #:))))))))))))))))))))))))))
-        print(self.nominalAtributos) #:))))))))))))))))))))))
-        
-        
-        
+        for key in keys:
+            for dict_key in self.diccionarios[key].keys():
+               self.datos[key] = self.datos[key].replace([dict_key], self.diccionarios[key][dict_key])
+
+        aux_dict_type = {}
+        for key in keys:
+            aux_dict_type[key] = float
+
+        self.datos=self.datos.astype(aux_dict_type)
     
     # Devuelve el subconjunto de los datos cuyos �ndices se pasan como argumento
     def extraeDatos(self,idx):
-        return self.datos.iloc[idx]
-
-
-"""if __name__ == "__main__":
-    
-    datos = Datos("./datasets/heart.csv")
-    print(datos.extraeDatos(0))"""
+        return [self.datos.iloc[i] for i in idx]
--- EstrategiaParticionado.py
+++ EstrategiaParticionado.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta,abstractmethod
 import random
+import numpy as np
 
 class Particion():
 
@@ -14,6 +15,7 @@ class EstrategiaParticionado:
   
   # Clase abstracta
   __metaclass__ = ABCMeta
+  particiones = []
   
   # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor 
   
@@ -27,23 +29,51 @@ class EstrategiaParticionado:
 
 class ValidacionSimple(EstrategiaParticionado):
   
+  proporcionTest = 0.0
+  numeroEjecuciones = 0
+
   # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el n�mero de ejecuciones deseado
+  def __init__(self, numeroEjecuciones: int, proporcionTest: float) -> None:
+    self.numeroEjecuciones = numeroEjecuciones
+    self.proporcionTest = proporcionTest
+    
+
   # Devuelve una lista de particiones (clase Particion)
   # TODO: implementar
   def creaParticiones(self,datos,seed=None):
-    
-    random.seed(seed)
-    pass
-      
+    for _ in range(0,self.numeroEjecuciones):
+      random.seed(seed)
+      part = Particion()
+      test = datos.sample(frac = self.proporcionTest)
+      train = datos.drop(test.index)
+
+      part.indicesTest.append(test.index)
+      part.indicesTrain.append(train.index)
+      self.particiones.append(part)
+            
       
 #####################################################################################################      
 class ValidacionCruzada(EstrategiaParticionado):
   
+  numeroParticiones = 0
+
   # Crea particiones segun el metodo de validacion cruzada.
+  def __init__(self, numeroParticiones: int) -> None:
+    self.numeroParticiones = numeroParticiones
+
   # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
   # Esta funcion devuelve una lista de particiones (clase Particion)
   # TODO: implementar
   def creaParticiones(self,datos,seed=None):   
     random.seed(seed)
-    pass
+    
+    sublists = np.array_split(datos, self.numeroParticiones)
+ 
+    for i in range(0,self.numeroParticiones):
+      part = Particion()
+      part.indicesTest.append(sublists[i].index)
+      part.indicesTrain.append(datos.drop(sublists[i].index).index)
+      self.particiones.append(part)
+
+
     
