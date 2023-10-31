120044d63d1601e011664e65674c4405616d99d1
RafaDom02 <rafadomsae@outlook.es>
2023-10-31 19:15:55 +0100

    Muy cerca de acabar

--- ClusteringKMeans.py
+++ ClusteringKMeans.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-class KMeans:
+class ClusteringKMeans:
     def __init__(self, k: int, max_iteraciones: int = 100):
         self.k = k
         self.max_iteraciones = max_iteraciones
@@ -49,23 +49,32 @@ if __name__ == '__main__':
     matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
 
-    np.random.seed(42)
+    from Datos import Datos
+
+    kmeans = ClusteringKMeans(k=3)
+
+    """np.random.seed(42)
     data1 = np.random.randn(50, 2)
-    data2 = np.random.randn(50, 2) + [5, 5]
-    data3 = np.random.randn(50, 2) + [10, -5]
+    data2 = np.random.randn(50, 2)
+    data3 = np.random.randn(50, 2)
 
     data = np.concatenate([data1, data2, data3])
     df = pd.DataFrame(data, columns=["x", "y"])
 
-    kmeans = KMeans(k=3)
-    clusters = kmeans.clasifica(df)
+    clusters = kmeans.clasifica(df)"""
+
+    dataset = Datos('./datasets/iris.csv')
+    clusters = kmeans.clasifica(dataset.datos.iloc[:,:-1])
+
+    for i in clusters:
+        print(i, "\n", clusters[i], "\n")
 
     colors = ['red', 'blue', 'green']
     for i, color in enumerate(colors):
         if clusters[i]:
             cluster_data = np.array(clusters[i])
-            plt.scatter(cluster_data[:, 0], cluster_data[:, 1], c=color, s=50)
+            plt.scatter(cluster_data[:, 2], cluster_data[:, 3], c=color, s=50)
 
     centroid_data = np.array(kmeans.centroids)
-    plt.scatter(centroid_data[:, 0], centroid_data[:, 1], c='black', s=200, marker='X')
+    plt.scatter(centroid_data[:, 2], centroid_data[:, 3], c='black', s=200, marker='X')
     plt.show()
